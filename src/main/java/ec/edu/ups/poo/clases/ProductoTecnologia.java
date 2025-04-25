ecba98bcc9e0bc967b9004ca44e7171c20b420ee
markzuck3rberg <141587136+markzuck3rberg@users.noreply.github.com>
2025-04-25 11:40:17 -0500

    feat(UPS-002): agregar agregar atributos y métodos de las clases Producto, ProductoAlimento, ProductoTecnología

--- src/main/java/ec/edu/ups/poo/clases/ProductoTecnologia.java
+++ src/main/java/ec/edu/ups/poo/clases/ProductoTecnologia.java
@@ -1,4 +1,23 @@
 package ec.edu.ups.poo.clases;
 
-public class ProductoTecnologia {
+public class ProductoTecnologia extends Producto{
+    private double garantia;
+    public ProductoTecnologia(String codigo, String nombre, double precioUnitario, Proveedor proveedor, double garantia) {
+        super(codigo, nombre, precioUnitario, proveedor);
+        this.garantia = garantia;
+    }
+    public double getGarantia() {
+        return garantia;
+    }
+    public void setGarantia(double garantia) {
+        this.garantia = garantia;
+    }
+    @Override
+    public double calcularCostoEspecial() {
+        if (garantia > 12) {
+            return getPrecioUnitario() * 1.10; // 10% más si tiene garantía extendida
+        } else {
+            return getPrecioUnitario();
+        }
+    }
 }
