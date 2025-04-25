33e59f3eb855cf6fe3dbdadab73c3efc35a369d3
jonnathans06 <jhonasaav@gmail.com>
2025-04-25 12:18:06 -0500

    feat(UPS-003): agregar metodos y atrubutos de las clases Persona, Proveedor y crear enums y interfaces

--- src/main/java/ec/edu/ups/poo/clases/Persona.java
+++ src/main/java/ec/edu/ups/poo/clases/Persona.java
@@ -1,4 +1,40 @@
 package ec.edu.ups.poo.clases;
 
 public class Persona {
+    private String idPersona;
+    private String nombre;
+    private String correo;
+
+    //Constructor
+    public Persona() {
+    }
+
+    public Persona(String idPersona, String nombre, String correo) {
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    //Getters y Setters
+
+    public String getIdPersona() {
+        return idPersona;
+    }
+    public void setIdPersona(String idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
